b0f45f36179947568e6d7f9f75ef99036c0d948b
Emanuelipa <106819344+Emanuelipa@users.noreply.github.com>
2024-09-26 20:45:17 -0300

    .

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -2,5 +2,8 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
 }
 
+vizualizarInformacoesGlobais()
